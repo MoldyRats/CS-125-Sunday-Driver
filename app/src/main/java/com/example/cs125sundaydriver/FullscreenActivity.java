6ee3d21f519e4e936e036d0e81ddff619e8e36fd
Adam <azatar3@illinois.edu>
2019-12-05 22:16:53 -0600

    i h8 android studio

--- app/src/main/java/com/example/cs125sundaydriver/FullscreenActivity.java
+++ app/src/main/java/com/example/cs125sundaydriver/FullscreenActivity.java
@@ -7,11 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -23,7 +25,10 @@ public class FullscreenActivity extends AppCompatActivity {
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
      */
     private static final boolean AUTO_HIDE = true;
-
+    /**
+     * Interstate driving animation.
+     */
+    AnimationDrawable interstateAnimation;
     /**
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
      * user interaction before hiding the system UI.
@@ -111,6 +116,10 @@ public class FullscreenActivity extends AppCompatActivity {
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
+        ImageView imageView = (ImageView)findViewById(R.id.image);
+        imageView.setBackgroundResource(R.drawable.interstateanimation);
+        interstateAnimation = (AnimationDrawable) imageView.getBackground();
+
 
         // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(new View.OnClickListener() {
@@ -124,6 +133,12 @@ public class FullscreenActivity extends AppCompatActivity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.ejectcarbutton).setOnTouchListener(mDelayHideTouchListener);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        interstateAnimation.start();
     }
 
     @Override
