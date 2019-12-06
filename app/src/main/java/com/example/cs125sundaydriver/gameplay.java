e30c37ab6a8cf83004c8feee13c93a1ea3dc38fa
Adam <azatar3@illinois.edu>
2019-12-05 23:33:58 -0600

    Version 2.1

--- app/src/main/java/com/example/cs125sundaydriver/gameplay.java
+++ app/src/main/java/com/example/cs125sundaydriver/gameplay.java
@@ -2,7 +2,10 @@ package com.example.cs125sundaydriver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class gameplay extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class gameplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay);
+        Button eject = findViewById(R.id.ejectcarbutton);
+        eject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CrashMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
